123069fc5045b3a88cb859f119c244c471ebe7fd
Zujaj <rules.asia@gmail.com>
2022-11-15 14:16:50 +0500

    feat: adds cypress grep

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,11 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import '@cypress/grep'
+
+// @ts-ignore
+import registerCypressGrep from '@cypress/grep'
+
+registerCypressGrep()
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
